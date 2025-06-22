e120747ebb84dfa8a8c8ec6d9250d90cd1c26c48
pjgowtham <pjgowtham@gmail.com>
2025-06-22 11:00:08 +0530

    UdfpsHelper: Track brightness mirror to hide/show Udfps dim layer
    This provides a much deeper integration with Udfps dim layer and
    the new brightness slider such that we don't need to look for APIs
    that can track temporary brightness. That is because when the
    brightness mirror is shown, there is no necessity for the Udfps
    dim layer to be present. This change also isn't functional with the
    legacy slider.
    
    It also fixes the issue where the dim layer is hidden with
    QS expansion -> Device Control where the alternate bouncer is brought up.
    
    Tests : Ensure that appropriate transparency of the dim layer is set
    when dragging or selecting the preferred brightness with the slider.
    
    Change-Id: I53f78cbcfd3b42a3b5dbe6110424687ebfac102d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Iverz <asterixiverz@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -112,6 +112,7 @@ import com.android.systemui.log.SessionTracker;
 import com.android.systemui.plugins.FalsingManager;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.power.domain.interactor.PowerInteractor;
+import com.android.systemui.settings.brightness.domain.interactor.BrightnessMirrorShowingInteractor;
 import com.android.systemui.shade.ShadeDisplayAware;
 import com.android.systemui.shade.domain.interactor.ShadeInteractor;
 import com.android.systemui.shared.system.SysUiStatsLog;
@@ -204,6 +205,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
     @NonNull private final SelectedUserInteractor mSelectedUserInteractor;
     private final boolean mIgnoreRefreshRate;
     private final KeyguardTransitionInteractor mKeyguardTransitionInteractor;
+    @NonNull private final BrightnessMirrorShowingInteractor mBrightnessMirrorShowingInteractor;
 
     // Currently the UdfpsController supports a single UDFPS sensor. If devices have multiple
     // sensors, this, in addition to a lot of the code here, will be updated.
@@ -376,7 +378,8 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                         mShadeInteractor,
                         mUdfpsOverlayInteractor,
                         mPowerInteractor,
-                        mScope
+                        mScope,
+                        mBrightnessMirrorShowingInteractor
                     )));
         }
 
@@ -788,7 +791,8 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             @NonNull PowerInteractor powerInteractor,
             @Application CoroutineScope scope,
             UserActivityNotifier userActivityNotifier,
-            @NonNull AuthController authController) {
+            @NonNull AuthController authController,
+            @NonNull BrightnessMirrorShowingInteractor brightnessMirrorShowingInteractor) {
         mContext = context;
         mExecution = execution;
         mVibrator = vibrator;
@@ -825,6 +829,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         mAlternateBouncerInteractor = alternateBouncerInteractor;
         mUdfpsOverlayInteractor = udfpsOverlayInteractor;
         mPowerInteractor = powerInteractor;
+        mBrightnessMirrorShowingInteractor = brightnessMirrorShowingInteractor;
         mScope = scope;
         mInputManager = inputManager;
         mSelectedUserInteractor = selectedUserInteractor;
